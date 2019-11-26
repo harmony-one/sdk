aeb46de5a586372164e5c4c5064f69f16b63d63a
Minh Doan <daywednes@gmail.com>
2019-11-26 10:30:23 -0800

    add staking_create

---
+++ examples/staking_create.js
@@ -0,0 +1,75 @@
+const { Harmony } = require('@harmony-js/core');
+const { ChainID, ChainType } = require('@harmony-js/utils');
+const {
+  StakingTransaction,
+  CreateValidator,
+  Delegate,
+  Undelegate,
+  CollectRewards,
+} = require('@harmony-js/staking');
+
+const createMsg = CreateValidator({
+  validatorAddress: 'one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9',
+  description: {
+    name: 'Alice',
+    identity: 'alice',
+    website: 'alice.harmony.one',
+    securityContact: 'Bob',
+    details: "Don't mess with me!!!",
+  },
+  commissionRates: {
+    rate: '0.1',
+    maxRate: '0.9',
+    maxChangeRate: '0.05',
+  },
+  minSelfDelegation: '0xa',
+  maxTotalDelegation: '0x0bb8',
+  slotPubKeys: [
+    '0xb9486167ab9087ab818dc4ce026edb5bf216863364c32e42df2af03c5ced1ad181e7d12f0e6dd5307a73b62247608611',
+  ],
+  amount: '0x64',
+});
+
+const stakingTxn = StakingTransaction({
+  directive: '0x',
+  stakeMsg: createMsg,
+  nonce: '0x2',
+  gasPrice: '0x',
+  gasLimit: '0x64',
+  chainId: 0,
+  from: 'one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9',
+});
+
+stakingTxn
+  .sendTransaction()
+  .then((stakingTxn, res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//   const delegateMsg = Delegate({
+//     delegatorAddress: 'one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9',
+//     validatorAddress: 'one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9',
+//     amount: '0xa',
+//   });
+
+//   const stakingTxn = StakingTransaction({
+//     directive: '0x2',
+//     stakeMsg: delegateMsg,
+//     nonce: '0x2',
+//     gasPrice: '0x',
+//     gasLimit: '0x64',
+//     chainId: 0,
+//     from: 'one1a0x3d6xpmr6f8wsyaxd9v36pytvp48zckswvv9',
+//   });
+
+//   stakingTxn
+//     .sendTransaction()
+//     .then((stakingTxn, res) => {
+//       console.log(res);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
